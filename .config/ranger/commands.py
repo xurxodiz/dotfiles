2686cf68d66820f6d5ced0b24a5ca598bfb798e6
Jorge Diz Pico <jdiz@tuenti.com>
2018-10-24 16:37:17 +0200

    Adds config for ranger (text-based file manager)

---
+++ .config/ranger/commands.py
@@ -0,0 +1,26 @@
+# From https://github.com/ranger/ranger/wiki/Commands
+
+import os;
+from ranger.api.commands import *
+
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+
+        fzf = self.fm.execute_command("fzf", stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
