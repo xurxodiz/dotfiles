e3e9336d94dda03be03248dbc92b750b75dd0e8f
Jorge Diz Pico <jorge.dizpico@mastercard.com>
2022-05-02 17:16:41 +0200

    Fix NVM config folder path

--- .config/zsh/.exports.zsh
+++ .config/zsh/.exports.zsh
@@ -34,7 +34,7 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export MCFLY_FUZZY=true
 
 # only effective if NVM exists
-export NVM_DIR="$XDG_CONFIG_HOME/.nvm"
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
